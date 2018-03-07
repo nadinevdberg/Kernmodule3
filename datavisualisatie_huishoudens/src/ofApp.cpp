e033f955e9b52c90bc2c2433fec47320e86d54ce
Nadine van den Berg <31652838+nadinevdberg@users.noreply.github.com>
2018-03-07 14:22:19 +0100

    Huishoudens
    inspiratie en code update

--- datavisualisatie_huishoudens/src/ofApp.cpp
+++ datavisualisatie_huishoudens/src/ofApp.cpp
@@ -1,71 +1,73 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	ofSetBackgroundColor(ofColor::black);
+	string databasePath = ofToDataPath("huishoudens.db", true);
+	db = new SQLite::Database(databasePath);
+
+
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+	SQLite::Statement query(*db, "SELECT * FROM huishoudens WHERE jaar=?");
 
+	int year = years[currentYear];
+	ofLog() << "year = " << year << endl;
+
+	query.bind(1, year);
+
+	ofDrawRectangle(100, 400, 50, 50);
+
+	while (query.executeStep()) {
+		ofLog() << query.getColumn("jaar") << endl;
+
+		hh_single = hh_single, query.getColumn("hh_single").getInt();
+
+	//	hh_single = ofLerp(hh_single, query.getColumn("hh_single").getInt(), 0.02);
+		ofSetColor(ofColor::lightBlue);
+		ofDrawRectangle(100, 400, 50, hh_single/1000);
+
+	
+
+	}
 }
 
-//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
 
-}
+
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
+	currentYear = ofMap(x, 0, ofGetWidth(), 0, 9);
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+
+
+
+
+/* dit werkte niet
+void ofMesh generatePieMesh(float totaal, float radius, float segments = 8) {  //totaal & radius wordt bepaald ahv totaal huishoudens, segmenten staan voor de andere waardes)
+	ofMesh mesh;
+	mesh.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+
+	//create mesh
+	mesh.addVertex(ofVec3f::zero());
+	mesh.add
+
+	//add centerpoint
+		mesh.addVerte
 
 }
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
+*/
--- datavisualisatie_huishoudens/src/ofApp.h
+++ datavisualisatie_huishoudens/src/ofApp.h
@@ -1,24 +1,42 @@
 #pragma once
 
 #include "ofMain.h"
+#include "SQLiteCpp.h"
+
+//bron https://gist.github.com/rc1/3715e673ef175b5e0c93
 
 class ofApp : public ofBaseApp{
 
+
+
 	public:
+
+
+
 		void setup();
 		void update();
 		void draw();
 
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+		void mouseMoved(int x, int y);
+	//	void generatePieMesh(float totaal, float radius, float segments);
+
+private:
+
+	int currentYear = 0;
+	int years[9] = { 1995, 2000, 2005, 2010, 2012, 2013, 2014, 2015, 2016 };
+
+	float totaalHuishoudens;  //wordt een groeiende cirkel
+	float hh_single;
+	float hh_multiple;
+	float hh_multiple_nokids;
+	float hh_multiple_kids;
+	float hh_multiple_notmarried;
+	float hh_multiple_married;
+	float hh_multiple_singleparrent;
+	float hh_multiple_other;
+
+	SQLite::Database* db;
 		
 };
+
