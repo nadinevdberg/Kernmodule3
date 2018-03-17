ccf7e633a9b5826d93c9aa6dd81c7be307c7ba99
Nadine van den Berg <31652838+nadinevdberg@users.noreply.github.com>
2018-03-17 14:27:59 +0100

    Prototype v1
    Basis opgezet

--- impact_v1/src/ofApp.cpp
+++ impact_v1/src/ofApp.cpp
@@ -1,71 +1,87 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	ofSetBackgroundColor(ofColor::black);
+	titleFont.load("SinkinSans-700Bold.otf", 30);
+	subtitleFont.load("SinkinSans-300Light.otf", 15);
+	baseFont.load("SinkinSans-200XLight.otf", 12);
+	baseFont.setLineHeight(25);
+	maxBarLength = 467;
+	minBarLength = 1;
+	currentSelfImageValue = 1;
+
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 
+
+	//Healthmonitor
+	ofFill();
+	ofSetColor(20, 20, 20);
+	ofDrawRectangle(50, 70, 550, 190); //BG
+	ofSetColor(191, 191, 191);
+	titleFont.drawString("HEALTH MONITOR", 87, 140);
+	baseFont.drawString("While we may not see what the impact of our words \nis on the outside,\
+ this monitor will show you what \nhappens on the inside.", 87, 165);
+
+	//Self Image
+	ofSetColor(20, 20, 20);
+	ofDrawRectangle(50, 291, 550, 160); //BG
+	ofSetColor(191, 191, 191);
+	subtitleFont.drawString("Self image", 87, 337);
+	ofNoFill();
+	ofDrawRectangle(87, 357, 475, 30); // placeholder progressbar
+	ofFill();
+	ofDrawRectangle(90, 359.5, currentSelfImageValue, 25); //dit zou de maximale grootte van de balk zijn, waarbij '470' van waarde gaat veranderen en bij 1 zal beginnen
+	baseFont.drawString("Positive", 87, 412);
+	baseFont.drawString("Negative", 487, 412);
+
+	//Depression
+	ofFill();
+	ofSetColor(20, 20, 20);
+	ofDrawRectangle(50, 483, 550, 160); //BG
+	ofSetColor(191, 191, 191);
+	subtitleFont.drawString("Depression", 87, 529);
+	ofNoFill();
+	ofDrawRectangle(87, 549, 475, 30); // placeholder progressbar
+	baseFont.drawString("Low", 87, 604);
+	baseFont.drawString("High", 500, 604);
+
+	//Performance
+	ofFill();
+	ofSetColor(20, 20, 20);
+	ofDrawRectangle(50, 675, 550, 160); //BG
+	ofSetColor(191, 191, 191);
+	subtitleFont.drawString("Performace", 87, 721);
+	ofNoFill();
+	ofDrawRectangle(87, 741, 475, 30); // placeholder progressbar
+	baseFont.drawString("Low", 87, 796);
+	baseFont.drawString("High", 500, 796);
+
+	//Image placeholder
+	ofFill();
+	ofSetColor(20, 20, 20);
+	ofDrawRectangle(640, 70, 1000, 765);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
+	if (currentSelfImageValue <= maxBarLength) {
+		if (key == ' ') {
+			currentSelfImageValue = ofLerp(currentSelfImageValue, currentSelfImageValue + 15, 0.2);
+			//currentSelfImageValue += 5;
 
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+			// Hoe werkt ofLerp ook alweer....
+		/*	newSelfImageValue = currentSelfImageValue += 5;
+			currentSelfImageValue = ofLerp(currentSelfImageValue, newSelfImageValue, 0.2);*/
+		}
+	}
 }
--- impact_v1/src/ofApp.h
+++ impact_v1/src/ofApp.h
@@ -10,15 +10,21 @@ class ofApp : public ofBaseApp{
 		void draw();
 
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+
+		int maxBarLength; 
+		int minBarLength;
+
+		float selfImageValue;
+		float currentSelfImageValue;
+		float newSelfImageValue;
+		float increment;
+
+
+private:
+	ofTrueTypeFont titleFont;
+	ofTrueTypeFont subtitleFont;
+	ofTrueTypeFont baseFont;
+
+
 		
 };
